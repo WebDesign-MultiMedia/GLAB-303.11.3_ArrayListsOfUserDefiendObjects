6abddc776e96de01b6bf1d969a520a3fe484607c
Julio <youremail@example.com>
2024-07-22 11:16:21 -0400

    comepleted

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
+import java.util.ArrayList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        //instantiation to class AddDataToArrayList
+        AddDataToArrayList b = new AddDataToArrayList();
+        ArrayList<Book> mybooklist = b.bookdetails();
+        for(Book showVal: mybooklist){
+            // invoke getter method for getting data---------
+            System.out.println(showVal.getNumber() + " " + showVal.getName() + " " + showVal.getCategory() + " " + showVal.getAuthor());
+        }
+
 
     }
 }
